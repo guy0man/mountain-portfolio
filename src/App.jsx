929334493818fe72c14f75be2f9f1d49839689d7
guy0man <martinbernardbondoc@gmail.com>
2026-03-19 06:21:34 +0800

    Fix:image size

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ function App() {
                   <img
                     src="/images/profile-pic.jpg"
                     alt="Person"
-                    className="h-200 w-full mb-3"
+                    className="max-h-200 w-full mb-3"
                   />
 
                   <p className='text-xl drop-shadow-sm text-white'>Martin Bernard Bondoc</p>
