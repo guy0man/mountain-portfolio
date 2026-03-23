36cdb56fd21bacd81b269850b7d2a2f3915c4249
guy0man <martinbernardbondoc@gmail.com>
2026-03-23 15:59:40 +0800

    Fix:build

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/mountain-portfolio/",
   plugins: [react()],
+  base: "/mountain-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
